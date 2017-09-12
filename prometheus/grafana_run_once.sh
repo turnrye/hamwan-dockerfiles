c3b18bc8de2dfa649c860daa05ae5886cba22afa
Ryan Turner <ryan.e.t@gmail.com>
2017-09-11 21:30:20 -0500

    Moved the grafana run once stuff to a separate image; calling that now; meant adding a new docker secret for our setup file

--- prometheus/grafana_run_once.sh
+++ prometheus/grafana_run_once.sh
@@ -1,9 +1,3 @@
-#!/bin/bash
-#Check for the touch file and exit early if it exists (that means this has already run)
-if [ -f /etc/opt/grafana_setup_run.touch ]; then
-    exit 1
-fi
-
 curl -v -k -X 'POST' -H "Content-Type: application/json" --user 'admin:admin' -d '{
   "name":"prometheus",
   "type":"prometheus",
@@ -11,8 +5,3 @@ curl -v -k -X 'POST' -H "Content-Type: application/json" --user 'admin:admin' -d
   "access":"proxy",
   "basicAuth":false
 }' http://localhost:3000/api/datasources
-
-#Prevent this from running again
-touch /etc/opt/grafana_setup_run.touch
-
-exit 1
